9c5ef8693485a61b513742891dbffd6b559a21a9
Bucko <bucko.perley@gmail.com>
2016-04-08 00:18:19 -0700

    Fixed routing and added signin layout for testing

--- client/routes.jsx
+++ client/routes.jsx
@@ -1,13 +1,61 @@
 import React, { Component } from 'react';
 import {mount} from 'react-mounter';
 import {Layout} from '../imports/ui/App.jsx';
+import {Signin} from '../imports/ui/Signin';
 import {Camera} from '../imports/ui/Camera';
 
 
 FlowRouter.route("/", {
+  name: 'Newsfeed',
+  action() {
+    mount(Layout, {
+      content: (<Newsfeed />)
+    });
+  }
+});
+
+FlowRouter.route("/signin", {
+  name: 'Signin',
+  action() {
+    mount(Layout, {
+      content: (<Signin />)
+    });
+  }
+});
+
+FlowRouter.route("/photo/:_id", {
+  name: 'PhotoViewer',
+  action() {
+    mount(Layout, {
+      content: (<PhotoViewer />)
+    });
+  }
+});
+
+FlowRouter.route("/map", {
+  name: 'Map',
+  action() {
+    mount(Layout, {
+      content: (<Map />)
+    });
+  }
+});
+
+FlowRouter.route("/camera", {
+  name: 'Camera',
   action() {
     mount(Layout, {
       content: (<Camera />)
     });
   }
 });
+
+FlowRouter.notFound = {
+  action() {
+    mount(Layout, {
+          content: (<NotFound />)
+        });
+  }
+};
+
+
