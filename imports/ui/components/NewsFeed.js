13f37b5f6e2f05d384e0eb8471b702960149be01
Tegan Duong <teganduong@gmail.com>
2016-04-11 19:53:44 -0700

    Add containers folder and create NewsFeedContainer file

--- imports/ui/components/NewsFeed.js
+++
@@ -1,6 +0,0 @@
-// NewsFeed component will contain NewsFeedEntry component, 
-// so that will be in this file rather than a separate file
-
-import React, { Component } from 'react';
-
-export class NewsFeed extends Component
