196347651a19557514c1a9c89f4659fba5457be7
Tegan Duong <teganduong@gmail.com>
2016-04-13 18:00:14 -0700

    Populate newsfeed w/ nearby posts once user's current location is determined

--- imports/ui/components/NewsFeed.jsx
+++ imports/ui/components/NewsFeed.jsx
@@ -7,35 +7,31 @@ import { createContainer } from 'meteor/react-meteor-data';
 export class NewsFeed extends Component {
   constructor(props) {
     super(props);
-    this.getLocation();
 
     this.state = {
       posts: []
     };
   }
 
-  getLocation() {
-    navigator.geolocation.getCurrentPosition((position) => {
-      renderPosts();
-    });
+  componentDidMount() {
+    this.renderNearbyPosts();
   }
 
-  renderPosts() {
+  renderNearbyPosts() {
     var self = this;
-    // Get current location first then call posts.nearby
-    var point = Geolocation.currentLocation() || { coords: { longitude: 0, latitude: 0 } };
-    console.log('====>>POINT', point);
-    Meteor.call('posts.nearby', point.coords.longitude,
-      point.coords.latitude, 300, 10,
-      function(err, result) {
-        // result is an array of post objects
-        console.log('====>', result);
-        self.setState({
-          posts: result
-        });
-      }
-    );
-    
+    // Get current location of user and once position is determined,
+    // call posts.nearby to populate newsfeed w/ nearby posts
+    navigator.geolocation.getCurrentPosition((position) => {
+      Meteor.call('posts.nearby', position.coords.longitude,
+        position.coords.latitude, 300, 10,
+        function(err, result) {
+          // result is an array of post objects
+          self.setState({
+            posts: result
+          });
+        }
+      );
+    });
   }
 
   render() {
