812c8584e0b8433944c73c2991025d8847d6a874
John Boucherie <jvboucherie@gmail.com>
2016-04-13 23:57:56 -0700

    Add a div with class bottomBuffer, pushing up content 50px, the size of the navbar.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,12 +5,12 @@ import { NewsFeed } from './components/NewsFeed'
 // define and export our Layout component
 export const Layout = ({content}) => (
     <div>
-        <h3 className='text-center'>Map My Moments</h3>
-        <hr />
-        <div className='row row-centered'>
+      <h3 className='text-center'>Map My Moments</h3>
+      <hr />
+      <div className='row row-centered'>
         <div className='col-lg-4 text-center col-centered'>{content}</div>
-        <div><NavBar /></div>
-        </div>
-
+          <div><NavBar /></div>
+        <div className='bottomBuffer'></div>
+      </div>
     </div>
 );
