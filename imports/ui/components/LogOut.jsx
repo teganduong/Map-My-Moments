a5b177eb19ea3eb3ac507295188c929974f1e61c
John Boucherie <jvboucherie@gmail.com>
2016-04-16 14:08:53 -0700

    Log out user after clicking button to confirm instead of on load.

--- imports/ui/components/LogOut.jsx
+++ imports/ui/components/LogOut.jsx
@@ -2,29 +2,31 @@ import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Signin } from '../Signin';
+
+
 
 export class Logout extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        loggedIn: null,
-      };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: null,
+    };
+  }
 
-    handleClick() {
-      Meteor.logout();
-    }
+  handleClick() {
+    Meteor.logout();
+    FlowRouter.go('Signin')
+  }
 
-    render() {
-      return (
-
-        <div>
+  render() {
+    return (
+      <div>
         Are you sure you want to log out?
         <br />
-        <a onClick={this.handleClick()} href='/signin'>Yeah, do it.</a>
-        </div>
-
-
-      )
-    }
+        <button onClick={this.handleClick}>Yeah, do it.</button>
+      </div>
+    )
+  }
 }
